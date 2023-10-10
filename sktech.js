d376a1880ebc592b2adf5bffda7995d5fd4030d0
thurrzx <146106203+thurrzx@users.noreply.github.com>
2023-10-10 11:45:57 -0300

    adicionando arquivo js ecss

---
+++ sktech.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 400);
+    background("red");
+  }
+  
+  function draw(){
+    
+    fill("black")
+    
+   if(mouseIsPressed) {
+  rect(mouseX,mouseY,30,30)
+  }}
